5c0f8a7a6711540f2239e33a370c49991af2eef2
Zach <zawood11@gmail.com>
2021-12-26 01:06:39 -0700

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/App";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
-
-ReactDOM.render(<Home />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
